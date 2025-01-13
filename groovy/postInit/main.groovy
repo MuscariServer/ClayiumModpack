ee69390dd466e378fcc75ca8a63b282d84327685
Tomotomo_ <55199184+Naini0712@users.noreply.github.com>
2025-01-13 21:38:21 +0900

    20250113

--- groovy/postInit/main.groovy
+++ groovy/postInit/main.groovy
@@ -1,7 +1,9 @@
 def recipeMap = mods.clayium.matter_transformer
 
+/*
 mods.clayium.matter_transformer.recipeBuilder()
     .input(ore('nuggetGold') * 9)
     .output(item('clayium:meta_ingot', 77))
     .CEt(30000).duration(200).tier(10)
     .buildAndRegister()
+*/
